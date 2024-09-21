b95187586d1a2230347f3e6e14c559519fb9b4f9
Nikoloz Taturashvili <tnicko@proton.me>
2024-09-21 04:11:20 +0400

    task 4 complete

---
+++ nikoloz-taturashvili-geolab/Homework-1/Task4.cs
@@ -0,0 +1,15 @@
+﻿#if TASK4_1
+Console.WriteLine("Please enter a number to check if it is a leap year or not");
+int year = int.Parse(Console.ReadLine());
+
+if (year % 4 == 0 && year % 100 != 0)
+{
+    Console.WriteLine($"{year} is a leap year");
+} else if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0)
+{
+    Console.WriteLine($"{year} is a leap year");
+} else
+{
+    Console.WriteLine($"{year} is not a leap year");
+}
+#endif
