ee7b522d1ace6aa7ef0eb0ed8b9c376d88983dfc
Nikoloz Taturashvili <tnicko@proton.me>
2024-09-21 04:11:05 +0400

    task 2 complete

---
+++ nikoloz-taturashvili-geolab/Homework-1/Task2.cs
@@ -0,0 +1,12 @@
+﻿#if TASK2_1
+Console.Write("Enter an integer: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine($"{num} is even");
+} else
+{
+    Console.WriteLine($"{num} is odd");
+}
+#endif
