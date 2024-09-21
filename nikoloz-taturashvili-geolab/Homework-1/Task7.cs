b3bd29576e494e454c50d249e8888aabd69b0058
Nikoloz Taturashvili <tnicko@proton.me>
2024-09-21 04:11:44 +0400

    task 7 complete

---
+++ nikoloz-taturashvili-geolab/Homework-1/Task7.cs
@@ -0,0 +1,35 @@
+﻿#if TASK7_1
+Console.Write("Enter number 1: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.Write("Enter the operator(+, -, *, /): ");
+string op = Console.ReadLine();
+
+
+Console.Write("Enter number 2: ");
+int num2 = int.Parse(Console.ReadLine());
+
+if (op == "+")
+{
+    Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+} else if (op == "-")
+{
+    Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+} else if (op == "*")
+{
+    Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+} else if (op == "/")
+{
+    if (num2 == 0)
+    {
+        Console.WriteLine("Can not divide by 0");
+    } else
+    {
+        Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+    }
+} else
+{
+    Console.WriteLine("Invalid operator!");
+}
+
+#endif
