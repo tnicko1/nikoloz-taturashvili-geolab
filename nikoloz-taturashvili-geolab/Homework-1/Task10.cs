f315a70ed349535a38ca2e4daa115fd52824f62f
Nikoloz Taturashvili <tnicko@proton.me>
2024-09-21 04:12:08 +0400

    task 10 complete

---
+++ nikoloz-taturashvili-geolab/Homework-1/Task10.cs
@@ -0,0 +1,21 @@
+﻿
+#if TASK10_1
+Console.Write("How much are you paying?: $");
+decimal pay = decimal.Parse(Console.ReadLine());
+if (pay < 0)
+{
+    Console.WriteLine($"You can not pay ${pay}");
+    return;
+}
+
+if (pay < 100)
+{
+    Console.WriteLine($"You paid {pay} (no discount)");
+} else if (pay <= 500)
+{
+    Console.WriteLine($"You paid {pay - (pay * 0.1m)} (10% discount)");
+}else
+{
+    Console.WriteLine($"You paid {pay - (pay * 0.2m)} (20% discount)");
+}
+#endif
