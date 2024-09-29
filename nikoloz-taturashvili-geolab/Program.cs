54eb5521d7b91c9d57b487ad12651d42c9a095be
Nikoloz Taturashvili <tnicko@proton.me>
2024-09-29 04:07:51 +0400

    added second namespace

--- nikoloz-taturashvili-geolab/Program.cs
+++ nikoloz-taturashvili-geolab/Program.cs
@@ -1,9 +1,9 @@
 ﻿using HW1 = nikoloz_taturashvili_geolab.Homework_1;
-
+using HW2 = nikoloz_taturashvili_geolab.Homework_2;
 class Program
 {
     static void Main(string[] args)
     {
-        HW1.Task1.Run();
+        HW2.BankTransactions.Run();
     }
 }
