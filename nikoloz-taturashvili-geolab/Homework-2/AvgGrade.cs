bd869cb1d677ad9fb45f2e94d3d62844fe3707a4
Nikoloz Taturashvili <tnicko@proton.me>
2024-09-29 04:07:14 +0400

    Practical Task 2 (Average Grade)

---
+++ nikoloz-taturashvili-geolab/Homework-2/AvgGrade.cs
@@ -0,0 +1,36 @@
+﻿namespace nikoloz_taturashvili_geolab.Homework_2;
+
+public class AvgGrade
+{
+    public static void Run()
+    {
+        Console.WriteLine("Enter your grades (at least 4)");
+        var count = 1d;
+        var sumOfGrades = 0d;
+        while (true)
+        {
+            Console.Write($"Enter grade {count}: ");
+            var grade = Convert.ToInt32(Console.ReadLine());
+            if (grade is < 0 or > 100)
+            {
+                Console.WriteLine("Grade out of range");
+                continue;
+            }
+            sumOfGrades += grade;
+            count++;
+            if (count > 4)
+            {
+                Console.WriteLine("Do you want to continue? (y/n)");
+                var input = Console.ReadLine()?.ToLower();
+                if (input is not ("y" or "yes"))
+                {
+                    break;
+                }
+            }
+        }
+        
+        var avgGrade = sumOfGrades / (count - 1);
+        Console.WriteLine($"Average grade: {avgGrade:F2}");
+        
+    }
+}
