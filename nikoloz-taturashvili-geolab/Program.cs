849045638305215bf60216ef63669f0b27fd4e00
Nikoloz Taturashvili <tnicko@proton.me>
2024-10-07 14:26:52 +0400

    Modified Program

--- nikoloz-taturashvili-geolab/Program.cs
+++ nikoloz-taturashvili-geolab/Program.cs
@@ -1,7 +1,7 @@
-﻿using Workshop = nikoloz_taturashvili_geolab.Workshop;
-using HW1 = nikoloz_taturashvili_geolab.Homework_1;
+﻿using HW1 = nikoloz_taturashvili_geolab.Homework_1;
 using HW2 = nikoloz_taturashvili_geolab.Homework_2;
 using HW3 = nikoloz_taturashvili_geolab.Homework_3;
+using HW4 = nikoloz_taturashvili_geolab.Homework_4;
 
 // ReSharper disable once IdentifierTypo
 namespace nikoloz_taturashvili_geolab;
@@ -10,6 +10,6 @@ internal abstract class Program
 {
     private static void Main(string[] args)
     {
-        HW3.Task3.Run();
+        HW4.Task3.Run();
     }
 }
