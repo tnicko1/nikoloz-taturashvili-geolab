3896a21b3c6ba376ed55f4cc8c33e8617214828e
Nikoloz Taturashvili <tnicko@proton.me>
2024-10-07 14:26:43 +0400

    Task 3

---
+++ nikoloz-taturashvili-geolab/Homework-4/Task3.cs
@@ -0,0 +1,45 @@
+﻿namespace nikoloz_taturashvili_geolab.Homework_4;
+
+internal enum Currency { Gel, Usd, Eur }
+
+internal class Product
+{
+    private string Name { get; }
+    private readonly decimal _price;
+
+    private decimal Price
+    {
+        get => _price;
+        init
+        {
+            _price = value switch
+            {
+                < 0 => 0,
+                _ => value,
+            };
+        }
+    }
+
+    private Currency Currency { get; }
+
+    public Product(string name, decimal price, Currency currency)
+    {
+        Name = name;
+        Price = price;
+        Currency = currency;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"{Name}: {Price} {Currency}");
+    }
+}
+
+public static class Task3
+{
+    public static void Run()
+    {
+        var product = new Product("Jelly Bean", 6, Currency.Gel);
+        product.Print();
+    }
+}
