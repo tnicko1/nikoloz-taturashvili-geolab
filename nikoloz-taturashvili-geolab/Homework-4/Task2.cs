5efa1b773300d8ad7852cae2de2f98a6fd152055
Nikoloz Taturashvili <tnicko@proton.me>
2024-10-07 14:26:35 +0400

    Task 2

---
+++ nikoloz-taturashvili-geolab/Homework-4/Task2.cs
@@ -0,0 +1,43 @@
+﻿namespace nikoloz_taturashvili_geolab.Homework_4;
+
+internal class Car
+{
+    private string Brand { get; }
+    private string Model { get; }
+    private readonly int _year;
+
+    private int Year
+    {
+        get => _year;
+        init
+        {
+            if (value < 1886)
+                _year = 1886;
+            else if (value > DateTime.Now.Year)
+                _year = DateTime.Now.Year;
+            else
+                _year = value;
+        }
+    }
+
+    public Car(string brand, string model, int year)
+    {
+        Brand = brand;
+        Model = model;
+        Year = year;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"{Brand} {Model} {Year}");
+    }
+}
+
+public static class Task2
+{
+    public static void Run()
+    {
+        var car = new Car("Audi", "RS7 Sport-back", 2024);
+        car.Print();
+    }
+}
